11fcc90ed9d2322679d4a9053aa7508b1d0be844
Shital Jante <shijante@in.ibm.com>
2021-04-05 21:17:15 +0530

    search node in linkedlist

--- linkedlist_func.go
+++ linkedlist_func.go
@@ -16,28 +16,62 @@ type linkedList struct {
 func main() {
 	linklist := linkedList{
 		&node{next: nil}}
+	fmt.Println("Insert Node at last: 10")
 	linklist.insertAtTail(10)
-	linklist.display()
+	fmt.Println("Insert Node at last: 20")
 	linklist.insertAtTail(20)
+	fmt.Println("Insert Node at front: 5")
+	linklist.insertAtFront(5)
+	fmt.Println("Insert Node at location 3")
+	linklist.insertAtFront(50)
+	linklist.insertAtGivenLocation(100, 3)
+	linklist.search(50)
 	linklist.display()
 
 }
 
-func (linklist *linkedList) insertAtTail(num int) {
+func (l *linkedList) insertAtTail(num int) {
 	n := node{value: num}
-	current := linklist.head
+	current := l.head
 	for current.next != nil {
 		current = current.next
 	}
 	current.next = &n
-	fmt.Println("Inserted Node")
 }
 
-func (linklist *linkedList) display() {
-	current := linklist.head.next
+func (l *linkedList) insertAtFront(num int) {
+	n := node{value: num}
+	tmp := l.head.next
+	l.head.next = &n
+	l.head.next.next = tmp
+}
+
+func (l *linkedList) display() {
+	current := l.head.next
 	for current != nil {
-		fmt.Print(current.value)
+		fmt.Print(current.value, " ")
 		current = current.next
 	}
-	fmt.Println()
+}
+
+func (l *linkedList) insertAtGivenLocation(num, location int) {
+	n := node{value: num}
+	current := l.head
+	for i := 1; i < location; i++ {
+		current = current.next
+	}
+	tmp := current.next
+	current.next = &n
+	current.next.next = tmp
+}
+
+func (l *linkedList) search(num int) {
+	current := l.head.next
+	for current != nil {
+		if current.value == num {
+			fmt.Println("found number")
+			break
+		}
+		current = current.next
+	}
 }
