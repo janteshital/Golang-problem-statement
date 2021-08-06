7524f7279226bc8217bcc40d6eaeebcf75899ccd
janteshital <janteshital263@gmail.com>
2021-08-06 15:17:31 +0530

    Create queue.go

---
+++ queue.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+type queue []string
+
+func main() {
+	fmt.Println("Hello, playground")
+	var newq queue
+	newq.enqueue("10")
+	newq.enqueue("20")
+	newq.enqueue("30")
+	newq.printStack()
+	newq.dequeue()
+	newq.printStack()
+	
+}
+
+func (q *queue)enqueue(n string){
+	*q = append(*q, n)
+}
+
+func (q *queue)printStack(){
+	fmt.Println(*q)
+}
+
+func (q *queue)dequeue(){
+	if len(*q) == 0 {
+		fmt.Println("no element")
+	}else {
+		*q = (*q)[1:] 
+	}
+	
+}
